860a7f6f9b544b9fce9ce11b8c7d62053cd61755
Rodrigo Nogueira <rdg6design@gmail.com>
2024-07-02 16:28:00 -0300

    prettier

--- src/App.jsx
+++ src/App.jsx
@@ -32,47 +32,47 @@ const App = () => {
 
   return (
     <AuthProvider>
-        <Header>
-          <NavBar />
-          <HeroSection />
-        </Header>
-        <main>
-          <MainSection />
-            <TaskList />
-          <GoodThingsSection />
-          <Form.Root>
-            <Form.Avatar />
-            <Form.Title />
-            <form className="contact-form__form" onSubmit={handleSubmit}>
+      <Header>
+        <NavBar />
+        <HeroSection />
+      </Header>
+      <main>
+        <MainSection />
+        <TaskList />
+        <GoodThingsSection />
+        <Form.Root>
+          <Form.Avatar />
+          <Form.Title />
+          <form className="contact-form__form" onSubmit={handleSubmit}>
+            <Form.InputGroup
+              label="Your name"
+              type="text"
+              placeholder="type your name here..."
+              required
+            />
+            <div className="contact-form__input-group contact-form__input-group--split">
               <Form.InputGroup
-                label="Your name"
-                type="text"
-                placeholder="type your name here..."
+                label="Email*"
+                type="email"
+                placeholder="example@example.com"
                 required
               />
-              <div className="contact-form__input-group contact-form__input-group--split">
-                <Form.InputGroup
-                  label="Email*"
-                  type="email"
-                  placeholder="example@example.com"
-                  required
-                />
-                <Form.InputGroup
-                  label="Phone*"
-                  type="tel"
-                  placeholder="( ) ____-____"
-                  required
-                />
-              </div>
-              <Form.Textarea
-                placeholder="Type what you want to say to us"
+              <Form.InputGroup
+                label="Phone*"
+                type="tel"
+                placeholder="( ) ____-____"
                 required
               />
-              <Form.SubmitButton>SEND NOW</Form.SubmitButton>
-            </form>
-          </Form.Root>
-        </main>
-        <Footer />
+            </div>
+            <Form.Textarea
+              placeholder="Type what you want to say to us"
+              required
+            />
+            <Form.SubmitButton>SEND NOW</Form.SubmitButton>
+          </form>
+        </Form.Root>
+      </main>
+      <Footer />
     </AuthProvider>
   );
 };
